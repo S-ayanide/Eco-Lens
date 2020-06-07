eb38209fbcb4757b07c3c5f359670b575aa1ac87
Sayan Mondal <sayanmondal342@gmail.com>
2020-06-07 11:09:15 +0530

    sqlite db added

---
+++ WebScraping/createStore.py
@@ -0,0 +1,54 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def main():
+
+    database = r".\Database\sqlite\scrapeSqlite.db"
+
+    sql_create_product_table = """ CREATE TABLE IF NOT EXISTS product (
+                                        id integer PRIMARY KEY,
+                                        image text,
+                                        name text NOT NULL,
+                                        url text NOT NULL                                        
+                                    ); """
+
+    # create a database connection
+    conn = create_connection(database)
+
+    # create tables
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_product_table)
+
+    else:
+        print("Error! cannot create the database connection.")
+
+
+if __name__ == '__main__':
+    main()
---
+++ WebScraping/insertSqlData.py
@@ -0,0 +1,63 @@
+import sqlite3
+import pickle
+from sqlite3 import Error
+
+# Loading Saved Dataset
+with open(r'./Data/item_details_url', 'rb') as item_data:
+    items = pickle.load(item_data)
+
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def insert_product(conn, product):
+    """
+    Create a new product into the product table
+    :param conn:
+    :param product:
+    :return: product id
+    """
+    sql = ''' INSERT INTO product(name,image,url)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, product)
+    return cur.lastrowid
+
+
+def fetchData():
+
+    # check the item list
+    for item in items:
+        print(item)
+        print("\n")
+
+
+def main():
+
+    database = r".\Database\sqlite\scrapeSqlite.db"
+
+    # create a database connection
+    conn = create_connection(database)
+
+    try:
+        with conn:
+            # add product items
+            for item in items:
+                product = (item['item_name'],
+                           item['item_image'], item['item_url'])
+                product_id = insert_product(conn, product)
+    except Error as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    main()
